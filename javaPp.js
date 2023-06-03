c21fcc331c3c0136ac7fbf2eaadf5fc618b12c68
gastoResler-Siste <resler.gaston@gmail.com>
2023-06-03 15:29:19 -0300

    vvccvbcvbcvb

---
+++ javaPp.js
@@ -0,0 +1,20 @@
+//java  hola
+
+
+
+$(document).ready(function(){
+     
+      
+    $("#nombre").click(function(){
+       // $("#nombre").val("otro texto")
+      
+       alert( $("#nombre").val())
+    });
+  
+  
+          //$("#mostrar").click(function(){
+          //  $("#elemento").text('otro texto');
+           //   $("#elemento").show();
+          
+      
+});
