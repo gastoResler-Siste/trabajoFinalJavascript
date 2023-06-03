55ead5963b49ebcf36e6867fbd0010a0344005ce
gastoResler-Siste <resler.gaston@gmail.com>
2023-06-03 18:27:03 -0300

    subi los ejemplos

---
+++ ejemplos clase/prueba_clase8.js
@@ -0,0 +1,21 @@
+//const mybutton=document.getElementById('my-button')
+//mybutton.addEventListener('click',)
+
+
+//este evento escucha        diapositiva (clase7.   pagina 15)
+/*    document.addEventListener('DOMContentLoaded',function(){
+        const boto=document.getElementById('id_my-button')
+        
+        boto.addEventListener('click', function(){
+            document.getElementById('texto_div').textContent ="rellenando el DIV"
+            document.getElementById('id_nombre').value=prompt("ingreser Un Nombre")
+            //lo mismo con JQuery
+        // $('id_nombre').val("contenido")
+
+        //oculto el text 2 
+        document.getElementById('id_nombre2').style.display="none";
+        //con el style le puedo dar estilo(color, tamaño)
+        })
+    })*/
+//otra forma de escuchar  diapositiva (clase7.   pagina 18)
+
