bed41fed677b0ecccb44aa8fb4a4be0574cfb413
gastoResler-Siste <resler.gaston@gmail.com>
2023-06-04 09:09:54 -0300

    s

--- javaPp.js
+++ javaPp.js
@@ -21,22 +21,24 @@ $(document).ready(function(){
       
    }
    //id="divRegistro"
-   
    $("#divReiniciar").show();
 
 
-    $("#botonComenzar").click(function(){
+   $("#botonComenzar").click(function(){
        // $("#nombre").val("otro texto")
        $("#divComenzar").hide();
        $("#divComenzar2").show();
 
-    });
+   });
+
     $("#botonAdivinanaza").click(function(){
       // $("#nombre").val("otro texto")
       $("#divComenzar2").hide();
-      $("#divPreguntas").show();
+      adivinanzas();
+      
 
    });
+
    $("#botonChoice").click(function(){
       // $("#nombre").val("otro texto")
       $("#divComenzar2").hide();
@@ -52,6 +54,33 @@ $(document).ready(function(){
           //$("#mostrar").click(function(){
           //  $("#elemento").text('otro texto');
            //   $("#elemento").show();
-          
-      
+   function adivinanzas(){
+      nroAdivinanza=0;
+      $("#divPreguntas").show();
+      let F_adiv =[{
+         pregunta:"¿Qué cosa es que cuanto más le quitas más grande es?",
+         opciones:["op1","op2","op3","op4"],
+         respuesta:2
+     }]  
+
+    // F_producto.push ({nombre:"Libro jardineris",precio:345,descuento:10,precioVenta: function(){precioVenta=parseFloat(this.precio)-((parseFloat(this.precio)* parseFloat(this.descuento))/100);return precioVenta;}})        
+   //id="idPadivinanza
+   }       
+   $("#botonSiguinte").click(function(){
+      // $("#nombre").val("otro texto")
+      siguinteAdivinanza()
+   });
+
+   function siguinteAdivinanza(){
+      nroAdivinanza++;
+               
+      $("#idPadivinanza").text(F_adiv[nroAdivinanza].pregunta);
+
+      alert(F_adiv[nroAdivinanza].opciones[2])
+
+
+
+   }
+
+
 });
