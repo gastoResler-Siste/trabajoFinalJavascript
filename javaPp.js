034428261ea65e411db88714ef7a873deb02a983
gastoResler-Siste <resler.gaston@gmail.com>
2023-06-04 01:59:05 -0300

    1 58 automatizando jquery
    automatizando ya

--- javaPp.js
+++ javaPp.js
@@ -7,18 +7,47 @@ $(document).ready(function(){
    let resultado=0  
    
    
-
-
-
-
+   const user=localStorage.getItem("usuario");
+   if (user==null){
+      localStorage.removeItem("usuario")
+      $("#divRegistro").show();
+      stop
+   
+   }
+   else {
+      
+      $("#pUsuario").text(user);
+      $("#divComenzar").show();
+      
+   }
+   //id="divRegistro"
+   
+   $("#divReiniciar").show();
 
 
     $("#botonComenzar").click(function(){
        // $("#nombre").val("otro texto")
-      
-       alert( "Bienvenido al Juego")
+       $("#divComenzar").hide();
+       $("#divComenzar2").show();
+
     });
-  
+    $("#botonAdivinanaza").click(function(){
+      // $("#nombre").val("otro texto")
+      $("#divComenzar2").hide();
+      $("#divPreguntas").show();
+
+   });
+   $("#botonChoice").click(function(){
+      // $("#nombre").val("otro texto")
+      $("#divComenzar2").hide();
+      $("#divPreguntas").show();
+
+   });
+    $("#grabar").click(function(){
+      // $("#nombre").val("otro texto")
+     
+      localStorage.setItem ("usuario","GASTON");
+   });
   
           //$("#mostrar").click(function(){
           //  $("#elemento").text('otro texto');
